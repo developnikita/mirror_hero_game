064900ad0cf7fdfb6a3ac8d8a5db839e43480d84
Nikita <nikitakorolov6@gmail.com>
2020-07-07 13:03:14 +0600

    [+] Реализовал первую версию класса BattleArena

--- src/main/java/com/neolab/heroesGame/arena/BattleArena.java
+++ src/main/java/com/neolab/heroesGame/arena/BattleArena.java
@@ -1,5 +1,19 @@
 package com.neolab.heroesGame.arena;
 
-public class BattleArena {
+import java.util.Map;
 
+public class BattleArena {
+    private Map<int, Army> armies;
+
+    public BattleArena(Map<int, Army> armies) {
+        this.armies = armies;
+    }
+
+    public boolean isArmyDied(int playerId) {
+        return armies.get(playerId).getHeroes().size() == 0;
+    }
+
+    public boolean isWarlordAlive(int playerId) {
+        return armies.get(playerId).isWarlordAlive();
+    }
 }
