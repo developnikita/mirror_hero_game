388ac68d71879c2f03f86d646c7ab1b46cdd7b5c
Nikita <nikitakorolov6@gmail.com>
2020-07-07 12:27:07 +0600

    [*] Update Army class
    Надо изменить структуру классов связанных с героями, потому что получится огромное количество дублирования кода. Зачем создавать новый класс для военнокомандующего, который ещё и наследуется от какого-то конкретного юнита, это вообще немного не клеется с логикой. Они отличаются только коэффициентом улучшения и для этого создавать отдельный класс. И выгоднее хранить военноначальника просто в армии так как он всё таки военноначальник, чтобы его не искать по списку.

--- src/main/java/com/neolab/heroesGame/arena/Army.java
+++ src/main/java/com/neolab/heroesGame/arena/Army.java
@@ -1,6 +1,9 @@
 package com.neolab.heroesGame.arena;
 
 import com.neolab.heroesGame.heroes.Hero;
+import com.neolab.heroesGame.heroes.WarlordFootman;
+import com.neolab.heroesGame.heroes.WarlordMagician;
+import com.neolab.heroesGame.heroes.WarlordVampire;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -39,11 +42,67 @@ public class Army {
 
     public boolean killHero(Hero hero) {
         removeAvailableHero(hero);
-        return  heroes.values().removeIf(value -> value.equals(hero));
+        return heroes.values().removeIf(value -> value.equals(hero));
     }
 
     public boolean removeAvailableHero(Hero hero) {
         return availableHero.values().removeIf(value -> value.equals(hero));
     }
 
+    public boolean isWarlordAlive() {
+        for (Hero h : heroes.values()) {
+            if (h.getClass().equals(WarlordFootman.class) ||
+                    h.getClass().equals(WarlordMagician.class) ||
+                    h.getClass().equals(WarlordVampire.class)) {
+                if (h.getHp() > 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void improveAllies() {
+        Optional<Hero> test = getWarlord();
+
+        if (test.isPresent()) {
+            if (test.get().getClass().equals(WarlordFootman.class)) {
+                WarlordFootman footman = (WarlordFootman) test.get();
+                heroes.values()
+                        .stream()
+                        .filter(h -> !h.getClass().equals(WarlordFootman.class))
+                        .peek(h -> improve(h, footman.getImproveCoefficient()));
+            } else if (test.get().getClass().equals(WarlordVampire.class)) {
+                WarlordVampire vampire = (WarlordVampire) test.get();
+                heroes.values()
+                        .stream()
+                        .filter(h -> !h.getClass().equals(WarlordVampire.class))
+                        .peek(h -> improve(h, vampire.getImproveCoefficient()));
+            } else if (test.get().getClass().equals(WarlordMagician.class)) {
+                WarlordMagician magician = (WarlordMagician) test.get();
+                heroes.values()
+                        .stream()
+                        .filter(h -> !h.getClass().equals(WarlordMagician.class))
+                        .peek(h -> improve(h, magician.getImproveCoefficient()));
+            }
+        }
+    }
+
+    private Optional<Hero> getWarlord() {
+        return heroes.values()
+                .stream()
+                .filter(h -> h.getClass().equals(WarlordFootman.class) ||
+                        h.getClass().equals(WarlordMagician.class) ||
+                        h.getClass().equals(WarlordVampire.class))
+                .findAny();
+    }
+
+    private void improve(Hero hero, float improveCoeff) {
+        int value = hero.getHp() + Math.round((float)hero.getHp() * improveCoeff);
+        hero.setHp(value);
+        value = hero.getDamage() + Math.round((float)hero.getDamage() * improveCoeff);
+        hero.setDamage(value);
+        float armor = (1 + improveCoeff) * hero.getArmor();
+        hero.setArmor(armor);
+    }
 }
